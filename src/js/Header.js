935a4f3eb2e399172585432b0e6f9162179a7c0d
Maksym <maximanbw@gmail.com>
2019-02-24 15:35:19 +0000

    Update style Header

--- src/js/Header.js
+++ src/js/Header.js
@@ -19,12 +19,14 @@ export class Header extends Component {
 			</div>
 		
 			<div className="phone">
-				<a href="/contact/#map">
-					<p>Товариська 56/Ладозька 25</p>
-					<p>Запоріжжя</p>
-				</a> 
 				<p><strong><a href="tel:+3 (8061) 707 37 11">тел. 707 37 11</a></strong></p>
 				<p><strong><a href="tel:+3 (8094) 920 77 11">моб. 094 920 77 11</a></strong></p>
+				<a href="/contact/#map">
+					<p><strong>Товариська 56/Ладозька 25</strong></p>
+				</a>
+				<a href="/contact/#map">
+					<p><strong>м.Запоріжжя</strong></p>
+				</a> 
 			</div>
 
 		</div>
